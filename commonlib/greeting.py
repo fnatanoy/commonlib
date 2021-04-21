9761d46b39135d6ea94a4838105207000fdbfa06
kenshoo <fnatanoy@gmail.com>
2021-04-21 16:57:22 +0300

    wip2

--- commonlib/greeting.py
+++ commonlib/greeting.py
@@ -12,3 +12,7 @@ def greeting_2(name: str):
 def greeting_3(name: str):
     """Print a simple greeting with the name."""
     print(f"Howdy 3, {name}")
+
+def greeting_4(name: str):
+    """Print a simple greeting with the name."""
+    print(f"Howdy 4, {name}")
